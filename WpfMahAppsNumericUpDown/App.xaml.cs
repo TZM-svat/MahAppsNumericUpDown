513953b9ae59523eac7eb532c1a60f327dbe69ed
TZM-svat <46556129+TZM-svat@users.noreply.github.com>
2019-01-10 09:49:39 +0100

    Add files via upload

---
+++ WpfMahAppsNumericUpDown/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WpfMahAppsNumericUpDown
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            var vm = new MainWindowViewModel();
+            var v = new MainWindow();
+            v.DataContext = vm;
+            v.ShowDialog();
+        }
+    }
+}
---
+++ WpfMahAppsNumericUpDown/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfMahAppsNumericUpDown
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _numericUpDownValue;
+        private double _textBoxValue;
+
+        public MainWindowViewModel()
+        {
+            NumericUpDownValue = 5;
+            TextBoxValue = 5;
+        }
+
+        public double NumericUpDownValue
+        {
+            get => _numericUpDownValue;
+            set
+            {
+                if (Math.Abs(_numericUpDownValue - value) < double.Epsilon) return;
+                _numericUpDownValue = value;
+                OnPropertyChanged(nameof(NumericUpDownValue));
+            }
+        }
+        public double TextBoxValue
+        {
+            get => _textBoxValue;
+            set
+            {
+                if (Math.Abs(_textBoxValue - value) < double.Epsilon) return;
+                _textBoxValue = value;
+                OnPropertyChanged(nameof(TextBoxValue));
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
